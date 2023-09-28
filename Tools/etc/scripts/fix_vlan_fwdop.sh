ff4ac0449e1a143beaff6c9d4357c6a9656a0df3
Anime4000 <ilham92_sakura@yahoo.com>
2023-09-29 01:38:51 +0800

    LUNA SDK can't use IFS=

--- Tools/etc/scripts/fix_vlan_fwdop.sh
+++ Tools/etc/scripts/fix_vlan_fwdop.sh
@@ -18,34 +18,36 @@ process_fwdop() {
 	entid="$1"
 	fwdop="$2"
 	active_id=$(omcicli mib get 84 | grep '$entid' | awk '{print $2}')
-	active_op=$(omcicli mib get 84 ${$entid} | grep '^FwdOp:' | awk '{print $2}')
+	active_op=$(omcicli mib get 84 "$entid" | grep '^FwdOp:' | awk '{print $2}')
 
-	# Check if user given value is exist or not
-	if [ -z "$active_id" ]; then
-		return 1
-	fi
-	
-	# Check FwdOp is same
-	if [[ $((fwdop)) -eq $((active_op)) ]]; then
-		return 0
-	fi
+    # Check if user given value is exist or not
+    if [ -z "$active_id" ]; then
+        return 1
+    fi
+    
+    # Check FwdOp is same
+    if [ "$fwdop" -eq "$active_op" ]; then
+        return 0
+    fi
 
-	# Run when user value is matched with the system
-	if [[ $((entid)) == $((active_id)) ]]; then
-		omcicli mib set 84 "${entid}" FwdOp "${fwdop}"
-	fi
+    # Run when user value is matched with the system
+    if [ "$entid" -eq "$active_id" ]; then
+        omcicli mib set 84 "$entid" FwdOp "$fwdop"
+    fi
 }
 
 # Function to process a pair
 process_pair() {
-	pair="$1"
-	# If the pair doesn't contain ':', add ':0x02'
-	if ! echo "$pair" | grep -q ":"; then
-		pair="$pair:0x02"
-	fi
-	
-	IFS=':' read -ra parts <<< "$pair"
-	process_fwdop "${parts[0]}" "${parts[1]}"
+    pair="$1"
+    # If the pair doesn't contain ':', add ':0x02'
+    if ! echo "$pair" | grep -q ":"; then
+        pair="$pair:0x02"
+    fi
+    
+    entid=$(echo "$pair" | awk -F':' '{print $1}')
+    fwdop=$(echo "$pair" | awk -F':' '{print $2}')
+    
+    process_fwdop "$entid" "$fwdop"
 }
 
 # Get the FwdOp fix from unused flash property: RTK_DEVINFO_SPECVER
@@ -57,9 +59,8 @@ if [ -z "$fix_vlan_fwdop" ]; then
 fi
 
 # Split the pairs by comma and process each pair
-IFS=',' read -ra pairs <<< "$fix_vlan_fwdop"
-for pair in "${pairs[@]}"; do
-	process_pair "$pair"
+echo "$fix_vlan_fwdop" | awk -F',' '{for(i=1;i<=NF;i++) print $i}' | while read -r pair; do
+    process_pair "$pair"
 done
 
 exit 0
